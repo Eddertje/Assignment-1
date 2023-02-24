d0787506df06cf4207bf7892d61b41366dbf3116
Toon Kling <t.kling@student.tudelft.nl>
2023-02-24 12:55:37 +0100

    Add deadsquare test from brightspace and empty implementation class

---
+++ src/agents/DeadSquareDetector.java
@@ -0,0 +1,9 @@
+package agents;
+
+import game.board.compact.BoardCompact;
+
+public abstract class DeadSquareDetector {
+    public static boolean[][] detect(BoardCompact board) {
+        return null;
+    }
+}
